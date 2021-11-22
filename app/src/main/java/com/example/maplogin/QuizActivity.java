f71fae29365142c1b8d6a83b475696afb3bba014
TÙNG PHAN NGUYỄN THANH <19120424@student.hcmus.edu.vn>
2021-11-22 11:13:13 +0700

    Add marker icon map

--- app/src/main/java/com/example/maplogin/QuizActivity.java
+++ app/src/main/java/com/example/maplogin/QuizActivity.java
@@ -8,8 +8,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.maplogin.utils.DatabaseAdapter;
-import com.example.maplogin.utils.Quiz;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class QuizActivity extends AppCompatActivity {
@@ -17,8 +17,9 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        setContentView(R.layout.activity_quiz_main);
 
+        ArrayList<String> questionIds = getIntent().getStringArrayListExtra("QUESTION_IDS");
         TextView tvUsername = findViewById(R.id.tvUsernameHome);
         CardView cvStartQuiz = findViewById(R.id.cvStartQuiz);
         CardView cvRule = findViewById(R.id.cvRule);
@@ -27,14 +28,17 @@ public class QuizActivity extends AppCompatActivity {
         String locationName = Objects.requireNonNull(db.getAllLocations().get(id)).name;
 
         tvUsername.setText(locationName);
+
         findViewById(R.id.imageViewBack).setOnClickListener(view -> finish());
 
         cvStartQuiz.setOnClickListener(view -> {
             Intent intent = new Intent(QuizActivity.this, Quiz.class);
+            intent.putStringArrayListExtra("QUESTION_IDS", questionIds);
+            intent.putExtra("LOCATION_ID", id);
             startActivity(intent);
         });
 
         cvRule.setOnClickListener(view ->
-                startActivity(new Intent(QuizActivity.this, RuleActivity.class)));
+            startActivity(new Intent(QuizActivity.this, RuleActivity.class)));
     }
 }
--- app/src/main/java/com/example/maplogin/utils/Quiz.java
+++
@@ -1,4 +0,0 @@
-package com.example.maplogin.utils;
-
-public class Quiz {
-}
